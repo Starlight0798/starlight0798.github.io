b99d09e5ce7b0063eb01574f8321e5262c05cd10
Starlight0798 <244057735@qq.com>
2024-03-24 17:45:26 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,8 @@
-# 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
 npm run build
-
-# 进入生成的文件夹
 cd .vuepress/dist
-
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# 如果发布到 https://Starlight0798.github.io
-# git push -f git@github.com:Starlight0798/Starlight0798.github.io.git master
-
-# 如果发布到 https://Starlight0798.github.io/<REPO>
-# git push -f git@github.com:Starlight0798/Starlight0798.github.io.git master:gh-pages
-# 我的配置如下
-git push -f https://github.com/Starlight0798/Starlight0798.github.io.git master:gh-pages
-
+git push -f https://github.com/Starlight0798/Starlight0798.github.io.git master
 cd -
