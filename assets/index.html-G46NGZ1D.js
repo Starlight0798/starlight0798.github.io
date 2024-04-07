94e29acbeda29dadfb2dd4f18ce69f033f8fffc1
Starlight0798 <Starlight0798@users.noreply.github.com>
2024-04-07 12:48:59 +0000

    deploy: fbcd1635eecff1778c9fb91f0485d194d7cf3c2d

---
+++ assets/index.html-G46NGZ1D.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-cdcb4f90","path":"/tags/language/1/","title":"","lang":"en-US","frontmatter":{"layout":"Categories"},"headers":[],"git":{},"filePathRelative":null}');export{t as data};
